7592f347931d961f878fce0c66640ba7a2ebab55
Michael Monda <michaeljmonda@outlook.com>
2023-10-12 14:48:29 -0400

    h

--- src/main.cpp
+++ src/main.cpp
@@ -121,7 +121,7 @@ void loop() {
     int inboundSignal = decoder.getKeyCode();   // when true, the key can be repeated if held down
     if (inboundSignal != -1) handleInbound(inboundSignal);  // inboundSignal == -1 only when unpressed
     Serial.println(inboundSignal);
-    switch(autoState) { // switching currState allows remote control, switching autoState does not.   
+    switch(currState) { // switching currState allows remote control, switching autoState does not.   
         case FOLLOWINGLINE:
             lineFollow(); // I don't use chassis.setTwist() because it's inconsistent
 
@@ -221,7 +221,9 @@ void loop() {
 
         case FOLLOWTODEPOT: // follow the line for a certain distance until we've reached the depot.
             lineFollow();   // follow the line (foolish samurai worrior)
-            if (chassis.getLeftEncoderCount() >= depotEncoderCount && chassis.getRightEncoderCount() >= depotEncoderCount) {
+            if (chassis.getLeftEncoderCount() >= depotEncoderCount && chassis.getRightEncoderCount() >= depotEncoderCount) {   // 4 cm
+                // chassis.getLeftEncoderCount() >= depotEncoderCount && chassis.getRightEncoderCount() >= depotEncoderCount
+                // ragefinder.getDistance() <= 2
                 chassis.setWheelSpeeds(0, 0);
                 currState = HALT;   // state change!
                 nextState = ZERO;   // prepare for plate deposit
